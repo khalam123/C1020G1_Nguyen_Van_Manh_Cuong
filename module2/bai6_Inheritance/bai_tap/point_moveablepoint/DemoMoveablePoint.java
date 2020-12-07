210242a3e11134715bf5c5cc85b5c44afd2d4a27
NguyenVanManhCuong <nvmacuong@gmail.com>
2020-12-07 14:21:32 +0700

    up

--- module2/bai6_Inheritance/bai_tap/point_moveablepoint/DemoMoveablePoint.java
+++ module2/bai6_Inheritance/bai_tap/point_moveablepoint/DemoMoveablePoint.java
@@ -7,15 +7,17 @@ import java.util.Arrays;
 public class DemoMoveablePoint {
     public static void main(String[] args) {
             MoveablePoint moveablePoint = new MoveablePoint();
-            MoveablePoint moveablePoint1 = new MoveablePoint(3,5,8);
+            MoveablePoint moveablePoint1 = new MoveablePoint(5,5);
 
             System.out.println("moveablePoint: "+moveablePoint);
             System.out.println("moveablePoint1: "+moveablePoint1);
+            System.out.println("moveablePoint1: "+moveablePoint1.move());
 
-            moveablePoint1.setXYZ(10,10,10);
+
+            moveablePoint1.setSpeed(10,10);
             System.out.println("moveablePoint1: " + moveablePoint1);
-            System.out.println();
-            System.out.println("moveablePoint1: "+ Arrays.toString(moveablePoint1.getXYZ()));
+        System.out.println();
+            System.out.println("moveablePoint1: "+moveablePoint1.move());
 
     }
 }
--- module2/bai6_Inheritance/bai_tap/point_moveablepoint/MoveablePoint.java
+++ module2/bai6_Inheritance/bai_tap/point_moveablepoint/MoveablePoint.java
@@ -1,33 +1,55 @@
 package bai6_Inheritance.bai_tap.point_moveablepoint;
 
 public class MoveablePoint extends Point {
-    private float z = 0.0f;
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
     public MoveablePoint(){
     }
 
-    public MoveablePoint(float x, float y, float z) {
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
         super(x, y);
-        this.z = z;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
 
-    public float getZ() {
-        return z;
+    public float getXSpeed() {
+        return xSpeed;
     }
 
-    public void setZ(float z) {
-        this.z = z;
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
     }
-    public float[] getXYZ(){
-        return new float[]{this.x,this.y,this.z};
+
+    public float getYSpeed() {
+        return ySpeed;
     }
-    public void setXYZ(float x, float y, float z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
     }
+    public float[] getSpeed(float xSpeed,float ySpeed){
+        return new float[]{this.xSpeed,this.ySpeed};
+    }
+    public void setSpeed(float xSpeed,float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public MoveablePoint move(){
+//        MoveablePoint moveablePoint = new MoveablePoint();
+        this.x += xSpeed;
+        this.y += ySpeed;
+        return this;
+    }
+
+
     @Override
     public String toString() {
-        return "("+this.x+" , "+this.y+" , " +this.z + " )";
+        return "("+this.x+" , "+this.y+")"+" , " +"speed = (" +this.xSpeed+" , "+this.ySpeed+ " )";
     }
 
 }
