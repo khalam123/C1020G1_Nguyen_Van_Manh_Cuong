f2098f9556d3ae7e8e622994e1c8ebad5b2dde8d
NguyenVanManhCuong <nvmacuong@gmail.com>
2021-01-12 11:08:54 +0700

    up

---
+++ module 3/bai3_queryDB/thuc_hanh/Bai3_thuc_hanh_1.sql
@@ -0,0 +1,13 @@
+-- sử dụng and
+select productCode, productName, buyprice, quantityInStock from products
+where buyprice > 56.76 and quantityInStock > 10;
+
+SELECT productCode,productName, buyprice, textDescription
+FROM products
+INNER JOIN productlines
+ON products.productline = productlines.productline
+WHERE buyprice > 56.76 AND buyprice < 95.59;
+-- sử dụng or
+select productCode, productName, buyprice, quantityInStock, productVendor, productLine 
+from products 
+where productLine = 'Classic Cars' or productVendor = 'Min Lin Diecast';
