3152187ab2a9e67c40baa9c21a8e9e578756ce6f
NguyenVanManhCuong <nvmacuong@gmail.com>
2021-01-26 14:01:55 +0700

    up

---
+++ module 3/CSS/_06_MVC/thuc_hanh/service/CustomerService.java
@@ -0,0 +1,13 @@
+package _06_mvc.service;
+
+import _06_mvc.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+    void save(Customer customer);
+    Customer findById(int id);
+    void update(int id, Customer customer);
+    void remove(int id);
+}
