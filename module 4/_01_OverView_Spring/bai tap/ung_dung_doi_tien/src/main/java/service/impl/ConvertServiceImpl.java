927b3673b1e65d372ec9efac2ab7d05071a7dc3f
NguyenVanManhCuong <nvmacuong@gmail.com>
2021-02-23 22:09:19 +0700

    up

---
+++ module 4/_01_OverView_Spring/bai tap/ung_dung_doi_tien/src/main/java/service/impl/ConvertServiceImpl.java
@@ -0,0 +1,13 @@
+package service.impl;
+
+import org.springframework.stereotype.Service;
+import service.ConvertService;
+
+@Service
+public class ConvertServiceImpl implements ConvertService {
+
+    @Override
+    public double convert(double money) {
+        return money * 23000;
+    }
+}
---
+++ module 4/_02_Spring_Controller/bai tap/May_tinh/src/main/java/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,27 @@
+package service.impl;
+
+import org.springframework.stereotype.Service;
+import service.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public double operator(double num1, double num2, String operator) {
+        double result = 0;
+        switch (operator){
+            case "Add(+)":
+                result = num1 + num2;
+                break;
+            case "Minus(-)":
+                result = num1 - num2;
+                break;
+            case "Multi(*)":
+                result = num1 * num2;
+                break;
+            case "Div(/)":
+                result = num1 / num2;
+                break;
+        }
+        return result;
+    }
+}
