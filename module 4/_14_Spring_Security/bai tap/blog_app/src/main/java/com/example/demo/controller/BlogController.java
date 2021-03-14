f159b8259f5250adc49543053050ab88546d59f6
NguyenVanManhCuong <nvmacuong@gmail.com>
2021-03-14 22:05:07 +0700

    up

--- module 4/_14_Spring_Security/bai tap/blog_app/src/main/java/com/example/demo/controller/BlogController.java
+++ module 4/_14_Spring_Security/bai tap/blog_app/src/main/java/com/example/demo/controller/BlogController.java
@@ -3,19 +3,19 @@ package com.example.demo.controller;
 import com.example.demo.model.Blog;
 import com.example.demo.service.BlogService;
 import com.example.demo.service.CategoryService;
+import com.example.demo.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 //@RestController
@@ -70,7 +70,12 @@ public class BlogController {
         return "/views/list";
     }
 
-
+    @GetMapping("/view/{id}")
+    public String showViewForm(@PathVariable int id,Model model){
+        Blog blog = blogService.findById(id);
+        model.addAttribute("blogs",blog);
+        return "/views/view";
+    }
 
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable int id, Model model){
@@ -102,4 +107,69 @@ public class BlogController {
         blogService.remove(blog.getId());
         return "redirect:/";
     }
+
+    @RequestMapping(value = { "/welcome" }, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "/views/welcomePage";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(Model model, Principal principal) {
+
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+
+        return "views/adminPage";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Model model) {
+
+        return "views/loginPage";
+    }
+
+    @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+    public String logoutSuccessfulPage(Model model) {
+        model.addAttribute("title", "Logout");
+        return "views/logoutSuccessfulPage";
+    }
+
+    @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+    public String userInfo(Model model, Principal principal) {
+
+        // Sau khi user login thanh cong se co principal
+        String userName = principal.getName();
+
+        System.out.println("User Name: " + userName);
+
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+
+        return "views/userInfoPage";
+    }
+
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied(Model model, Principal principal) {
+
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+            String userInfo = WebUtils.toString(loginedUser);
+
+            model.addAttribute("userInfo", userInfo);
+
+            String message = "Hi " + principal.getName() //
+                    + "<br> You do not have permission to access this page!";
+            model.addAttribute("message", message);
+
+        }
+        return "views/403Page";
+    }
+
 }
